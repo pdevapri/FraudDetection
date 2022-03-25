a592df4a68f6984b7e88e1ebb999d02f8957e78f
pdevapri <pallavi.debapriya17@gmail.com>
2022-03-25 13:09:21 +0530

    added 3 new model code

--- ml_model.py
+++ ml_model.py
@@ -2,3 +2,12 @@ ml_model.py
 
 
 print("adding all ml realted code here)
+
+def model1()
+    print("adding code for ML model1")
+
+def model2()
+    print("adding code for ML model3")
+
+def model3()
+    print("adding code for ML model3")
